4938f419cd8023ce26431b547b45eff6d6dd0bc2
pandomonium <53186853+pandomonium@users.noreply.github.com>
2021-02-08 22:20:01 -0700

    Added comments to the JavaScript of the sales calculator
    Issue #2

--- scripts/salescalc.js
+++ scripts/salescalc.js
@@ -1,9 +1,11 @@
+// This prorgam will calculate sales using function CalculateSales
 addEventListener("load", main)
 
+// This function contains an EventListener
 function main() {
     btCalcSales.addEventListener("click", calculateSales)
 }
-
+// This function will calculate the Sales.
 function calculateSales() {
     Price=parseFloat(txPrice.value)
     TaxRate=parseFloat(txTaxRate.value)/100
